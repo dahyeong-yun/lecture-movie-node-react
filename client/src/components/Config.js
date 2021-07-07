29615999fb9311533be73a095d46071e7eee5ce6
danny <gryb809@gmail.com>
2021-07-07 15:45:23 +0900

    :sparkles: 랜딩 페이지 메인 컴포넌트 추가

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,3 +1,6 @@
 export const USER_SERVER = '/api/users'
-export const API_URL = 'https://api.themoivedb.org/3'
-export const IMAGE_BASE_URL = 'http://image.tmdb.org/'
+
+export const API_URL = 'https://api.themoviedb.org/3'
+export const API_KEY = '3e19744b78820cc683059e8847e67dba'
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p'
+
